f5647c51bf22abade0a6fb10d9434d76ba56d58a
Hoon Lee <yl.hoony@gmail.com>
2017-09-30 08:14:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,16 +2,10 @@
 
 const drivers = ["Milo", "Otis", "Garfield"];
 
+function destructivelyAppendDriver(name) {
+  return drivers.push(name);
+}
 
-// describe('drivers', function () {
-//   it('is assigned an initial value of ["Milo", "Otis", "Garfield"]', function () {
-//     expect(drivers).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
-//   });
-//
-//   it('is declared with const', function () {
-//     expect(function () { drivers = []; }).to.throw(TypeError);
-//   });
-// });
 //
 // describe('Array functions', function () {
 //   beforeEach(function () {
